9e1f1fd0b5b04f672c88c04c0f4d4ffdb108efdf
hassan <hhadihasan288@gmail.com>
2023-11-12 16:30:37 +0100

    premier commite

---
+++ drupal/entrypoint2.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+# Waiting for mysql container to be setup
+echo "Waiting for mysql container..."
+
+# Changer de méthode. Requete http ????????
+sleep 35
+
+cd /opt/drupal
+
+# Perform Drupal site installation using Drush
+drush site-install demo_umami --account-pass=${DRUPAL_PASSWORD} --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mysql/${MYSQL_DATABASE} -y
+
+# Installs modules
+drush pm:install jsonapi
+drush pm:install basic_auth
+drush config:delete jsonapi.settings read_only
+
+apache2-foreground
